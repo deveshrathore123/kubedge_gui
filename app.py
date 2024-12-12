1590b7152790d673d4fe325505f3bd4cf7b9f30b
deveshrathore123 <rathoredeveshsingh6@gmail.com>
2024-12-11 22:20:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 def static_page():
     return render_template('static/static.html')
 
-# New routes for static website sub-pages
+# Static sub-pages
 @app.route('/static/html')
 def static_html():
     return render_template('static/html.html')
@@ -23,6 +23,24 @@ def static_html_css():
 @app.route('/static/html-css-js')
 def static_html_css_js():
     return render_template('static/html_css_js.html')
+
+# Route for dynamic website technologies
+@app.route('/dynamic')
+def dynamic_page():
+    return render_template('dynamic/dynamic.html')
+
+# Dynamic sub-pages
+@app.route('/dynamic/frontend')
+def dynamic_frontend():
+    return render_template('dynamic/frontend.html')
+
+@app.route('/dynamic/backend')
+def dynamic_backend():
+    return render_template('dynamic/backend.html')
+
+@app.route('/dynamic/additional-technologies')
+def dynamic_additional_technologies():
+    return render_template('dynamic/additional_technologies.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
