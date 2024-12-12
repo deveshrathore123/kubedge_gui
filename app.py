58cf77cd964bdc76dfb3f8574f2ed264f34510d8
deveshrathore123 <rathoredeveshsingh6@gmail.com>
2024-12-11 20:49:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Route for the main page
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 # Placeholder route for hosting static web
 @app.route('/host-static')
